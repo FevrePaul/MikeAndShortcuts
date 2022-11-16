0686185613f4c7a29d3ddae4a6d3958816d537a9
Paul Fevre <paul.fevre0@gmail.com>
2022-11-16 17:15:51 +0100

    Solution works with Pypy, removing double walking forward

--- solution.py
+++ solution.py
@@ -1,32 +1,22 @@
 #!/bin/python3
 
-
 def process_shortcut( val, index, ans ):
     start = ans[index]
-    lowerIdx, higherIdx = index - 1, index + 1
-
+    lowerIdx = index - 1
     lowerStop = False
-    higherStop = False
 
-    while lowerIdx > 0 or higherIdx < len(ans) - 1:
+    while lowerIdx > 0:
         if lowerIdx > 0 and ans[lowerIdx] > val:
             ans[lowerIdx] = val
             lowerIdx -= 1
-        elif not lowerStop:
-            lowerStop = True
-        if higherIdx < len(ans)-1 and ans[higherIdx] > val:
-            ans[higherIdx] = val
-            higherIdx += 1
-        elif not higherStop:
-            higherStop = True
-
+        else:
+            break
         val += 1
 
-        if higherStop and lowerStop:
-            break
     return ans
 
 def process_intersections():
+    # Process input
     n = int( input() )
     shortcuts = [ int( x ) for x in input().split() ]
 
@@ -39,10 +29,11 @@ def process_intersections():
         # Indexing is not the same for shortcut and i
         if i > 0 and ( ans[i-1] + 1 ) < ans[i]:
             ans[i] = ans[i-1]+1
-        if i < j - 2:
-            if ans[j-1] > ans[i] + 1:
-                ans[j-1] = ans[i] + 1
-                ans = process_shortcut( ans[j-1] + 1, j-1, ans )
+
+        # Check if we really would win anything by going through the shortcut
+        if i < j - 2 and ans[j-1] > ans[i] + 1:
+            ans[j-1] = ans[i] + 1
+            ans = process_shortcut( ans[j-1] + 1, j-1, ans )
     print( *ans )
 
 
